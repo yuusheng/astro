dcf991626dd390810f6c153e57c15fa6fad0eefa
余声里 <64511565+yuusheng@users.noreply.github.com>
2022-09-30 23:24:42 +0800

    feat: add unocss

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
 import solidjs from '@astrojs/solid-js'
+import Uno from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidjs()],
+  integrations: [solidjs(), Uno()],
 })
